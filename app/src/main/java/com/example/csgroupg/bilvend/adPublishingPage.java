4ab0dd976ad5ec53ca2ac4b417ba7ced027d1d3a
JavidHaji-zada <33520999+JavidHaji-zada@users.noreply.github.com>
2018-04-26 16:50:01 +0300

    -Advertisement Publishing Page uploaded

--- app/src/main/java/com/example/csgroupg/bilvend/adPublishingPage.java
+++ app/src/main/java/com/example/csgroupg/bilvend/adPublishingPage.java
@@ -1,11 +1,16 @@
 package com.example.csgroupg.bilvend;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Patterns;
@@ -17,7 +22,26 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class adPublishingPage extends AppCompatActivity{
@@ -31,12 +55,17 @@ public class adPublishingPage extends AppCompatActivity{
     private EditText priceOfItem;
     private EditText description;
     private View mDividerMode;
-    private String title;
-    private double price;
-    private String contentInfo;
     private ArrayList<EditText> list;
     private Advertisement mItem;
     private Spinner spinner;
+    private int REQUEST_CAMERA = 1;
+    private int SELECT_FILE = 0;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private DatabaseReference mReference;
+    private int  ID;
+    private StorageReference mStorageRef;
+    private Uri mUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +73,24 @@ public class adPublishingPage extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ad_publishing_page);
         Toolbar toolbar = findViewById(R.id.toolbar);
+
+        mAuth =FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        mReference = FirebaseDatabase.getInstance().getReference().child("Advertisements");
+
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ID = Integer.parseInt(String.valueOf(dataSnapshot.child("Total number of advertisements").getValue(Long.class))) + 1;
+
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         publish = findViewById(R.id.finishPublish);
         camera = findViewById(R.id.camera);
         imageOfItem = findViewById(R.id.photo);
@@ -78,7 +125,7 @@ public class adPublishingPage extends AppCompatActivity{
                     camera.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
-                            startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE),0);
+                            selectImage();
                         }
                     });
                 }
@@ -104,7 +151,8 @@ public class adPublishingPage extends AppCompatActivity{
             public void onClick(View v) {
                if (checkFilled(list))
                {
-               //    publishAdvertisement();
+                   publishAdvertisement();
+                   startActivity(new Intent(getApplicationContext(),mainPage.class));
                }
             }
         });
@@ -122,10 +170,75 @@ public class adPublishingPage extends AppCompatActivity{
         }
         return true;
     }
-//    public void publishAdvertisement()
-//    {
-//        mItem = new Item(nameOfItem.getText().toString(), id, spinner.g
-//        String name, int ID, int categoryType, Date uploadDate, ImageView advertisementImage
-//    }
+    public void publishAdvertisement()
+    {
+        Date c = Calendar.getInstance().getTime();
+        mItem = new Item(nameOfItem.getText().toString(), ID, spinner.getSelectedItemPosition(), c,imageOfItem);
+        mReference = FirebaseDatabase.getInstance().getReference().child("Advertisements").child(("ID number " + ID +""));
+        Map newPost = new HashMap();
+        newPost.put("Title", nameOfItem.getText().toString());
+        newPost.put("Publisher ID",user.getUid());
+        newPost.put("Category", spinner.getSelectedItem());
+        newPost.put("Upload date", c);
+        mReference.setValue( newPost);
+        mReference = FirebaseDatabase.getInstance().getReference().child("Advertisements").child("Total number of advertisements");
+        mReference.setValue(ID);
+        mStorageRef = FirebaseStorage.getInstance().getReference().child("Photos/" + user.getUid() + "/" + nameOfItem.getText().toString() + ".jpg");
+        mStorageRef.putFile(mUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Uri downloadUrl = taskSnapshot.getDownloadUrl();
+            }
+        });
+
+
+    }
+    public void selectImage()
+    {
+        final String[] items = {"Camera", "Gallery","Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Add image of the item");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which == 0)
+                {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(intent,REQUEST_CAMERA);
+                }
+                else if(which == 1)
+                {
+                    Intent intent =new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    intent.setType("image/*");
+                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+                }
+                else if(which == 2)
+                {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode == Activity.RESULT_OK )
+        {
+            if ( requestCode == REQUEST_CAMERA)
+            {
+                Bundle bundle = data.getExtras();
+                Bitmap bmp = (Bitmap) bundle.get("data");
+                imageOfItem.setImageBitmap(bmp);
+                imageOfItem.setImageURI(data.getData());
+            }
+            else if ( requestCode == SELECT_FILE)
+            {
+                mUri = data.getData();
+                imageOfItem.setImageURI(mUri);
+            }
+        }
+    }
 
 }
